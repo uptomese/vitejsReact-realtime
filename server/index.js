7b2d39a5b8bd15fdc095df81547fd3a5c4d4d359
uptomese <keattisak.phompoung@gmail.com>
2023-06-23 17:12:53 +0700

    2 files modified, 3 added

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+//New imports
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
